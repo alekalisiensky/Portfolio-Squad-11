412e18c7aabe2831597c5e331fb64996892e1703
desmazelada <duda2010bernardi@gmail.com>
2021-09-05 03:33:58 -0300

    menu dropdown adicionado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//script pra pagina scrollar até a âncora
 $(document).on("click", 'a[href*="#"]', function(event) {
     
     event.preventDefault();
@@ -8,3 +9,75 @@ $(document).on("click", 'a[href*="#"]', function(event) {
         scrollTop: targetOffset - 100}, 500
     );
 });
+
+//script pra nav virar um menu
+var largura = window.innerWidth
+    document.documentElement.clientWidth;
+    document.body.clientWidth;
+;
+var nav = document.querySelector(".topo-menu-invisivel");
+var botaoMenu = document.querySelector("button");
+var menu = document.querySelector("menu");
+var menuVisivel = false;
+
+console.log(largura);
+criaMenu();
+
+//evento pra identificar quando a aba do navegador for redimensionada
+window.dispatchEvent(new Event('resize'));
+window.addEventListener("resize", function(){
+    largura = window.innerWidth;
+    console.log(largura);
+
+    criaMenu();
+    console.log(menu.classList);
+});
+
+function criaMenu(){
+    if(largura <= 930){
+        menuVisivel = true;
+        nav.classList.add("menu");
+        menu.classList.remove("opcoes-menu")
+        menu.classList.add("opcoes-invisiveis");
+    }else if(largura > 930){
+        menuVisivel = false;
+        nav.classList.remove("menu");
+        menu.classList.remove("opcoes-visiveis");
+        menu.classList.remove("opcoes-invisiveis");
+        menu.classList.add("opcoes-menu");
+    }
+}
+
+//script pro menu descer e subir quando o botão for clicado
+var numCliques = 0;
+
+function mostraMenu(){
+    incrementaClique();
+    //mostra o menu quando o botao é clicado pela primeira vez e esconde na segunda
+    if(numCliques%2 != 0){
+        menu.classList.remove("opcoes-invisiveis");
+        menu.classList.toggle("opcoes-visiveis");
+    }else{
+        menu.classList.remove("opcoes-visiveis");
+        menu.classList.add("opcoes-invisiveis");
+    }
+}
+
+function incrementaClique(){
+    contaClique(++numCliques)
+}
+
+function contaClique(clique){
+    clique = botaoMenu.innerHTML;
+}
+
+
+//script pro menu sumir quando outro lugar da pagina for clicado
+window.onclick = function(event){
+    console.log(menuVisivel)
+    if (!event.target.matches('.botao-menu') && menuVisivel){
+        menu.classList.remove("opcoes-visiveis");
+        menu.classList.add("opcoes-invisiveis");
+        numCliques = 0;
+    }
+}
